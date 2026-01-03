e24cdbf806dbf9ea484eb5c25072128bd0a3d2d0
KrPrince19 <princekumarydv19@gmail.com>
2026-01-03 21:42:46 +0530

    Update admindashboard.js

--- admin/app/components/admindashboard.js
+++ admin/app/components/admindashboard.js
@@ -40,7 +40,7 @@ export default function DynamicFormUploader() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/tournament", {
+      const response = await fetch("https://bgmibackend.vercel.app/tournament", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ collection, data: [formData] }),
